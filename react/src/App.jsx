6e2ed4705460a6be6eaaa4a85f3bd13495547d69
Anthony Legere <anthonyclegere@gmail.com>
2024-05-22 14:41:05 -0400

    added front end routes

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,25 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import Home from "./Home";
 import "./App.css";
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 /*import LoginSignUp from "./components/LoginSignUp";*/
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Home from "./Home";
+import Men from "./components/Men";
+import Women from "./components/Women";
+import Unisex from "./components/Unisex";
+import Accessories from "./components/Accessories";
 
-function App() {
+const App = () => {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
+        <Route path="/unisex" element={<Unisex />} />
+        <Route path="/accessories" element={<Accessories />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
-}
+};
 
 export default App;
