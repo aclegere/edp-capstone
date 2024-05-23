a75cbc3a4c653570a1d2c2db1bed73dfc3f7bb10
Alyssa Navarro <alyssa@webage.com>
2024-05-23 11:33:13 -0400

    created form on checkout page

--- react/src/components/Checkout.jsx
+++ react/src/components/Checkout.jsx
@@ -2,23 +2,37 @@ import React from 'react';
 import './Checkout.css';
 
 const Checkout = () => {
+
+
+
     return (
-      <div className = 'form-box'>
-        <h5 className ='form-step'>Shipping Information</h5>
-        <form>
-          <div className ="field1">
-            <label>customer info</label>
-            <input placeholder = 'Name' />
-            <input placeholder='Phone 000-000-0000' />
-            <input placeholder = 'E-mail' />
-            <textarea placeholder='Shipping Address' />
-          </div>
+      <>
+        <div className="form-box">
+          <h5 className="form-step">Shipping Information</h5>
+          <form>
+            <div className="field1">
+              <label>customer info</label>
+              <input placeholder="Name" />
+              <input placeholder="Phone 000-000-0000" />
+              <input placeholder="E-mail" />
+              <textarea placeholder="Shipping Address" />
+            </div>
+          </form>
+        </div>
 
-          <button type='submit' id='submitBtn' className='submitBtn'>submit</button>
-        </form>
-
-      </div>
-      
+        <div className="payment-box">
+          <form>
+            <div className="field1">
+              <label>Payment Info</label>
+              <input placeholder="Name on Card" />
+              <input placeholder="Card Number" />
+              <input placeholder="Expiration Date (MM/YY)"/>
+              <input placeholder="Security Code" />
+            </div>
+          </form>
+          <button className="submitBtn" type="submit">Submit</button>
+        </div>
+      </>
     );
 }
 
--- react/src/components/Footer.jsx
+++ react/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './FooterStyle.css';
 
 const Footer = () => {
     return (
